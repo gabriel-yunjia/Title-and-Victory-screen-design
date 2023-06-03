be6336f8b205e386edcc35c1876bea5c9e8dcb25
gabriel-yunjia <gli79@ucsc.edu>
2023-06-02 22:05:34 -0700

    Update Scene1.js

--- scenes/Scene1.js
+++ scenes/Scene1.js
@@ -2,8 +2,7 @@ class Scene1 extends Phaser.Scene {
     constructor() {
         super('Scene1');
     }
-    waterBoy;
-    gameEnded = false;
+   
     create() {
         this.add.text(game.config.width/2, 260, 'Title and Victory screen design (WIP)', { font: '24px Futura', fill: '#FFFFFF' }).setOrigin(0.5);
 
